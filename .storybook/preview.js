855300a7e59700d1c93b5b3c9475b231dab45649
Daniel <2005danielus@gmail.com>
2022-02-05 11:13:43 +0100

    comments

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,4 +6,12 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  comments: {
+    apiKey: 'AIzaSyBwxwnsVPzHQPmx7E2tA0ZVA08jxL2w0_Q',
+    authDomain: 'dchat-c1c49.firebaseapp.com',
+    databaseURL: 'https://dchat-c1c49-default-rtdb.firebaseio.com',
+    projectId: 'dchat-c1c49',
+    storageBucket: 'dchat-c1c49.appspot.com',
+    messagingSenderId: '43579537838',
+  },
 }
