ca7b1e5701669e6ca8cf8d7f87e0762bce54dfee
Daniel <2005danielus@gmail.com>
2022-02-05 11:25:31 +0100

    revert

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,12 +5,5 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
-  },
-  darkMode: {
-    // Override the default dark theme
-    dark: { ...themes.dark, appBg: 'black' },
-    // Override the default light theme
-    light: { ...themes.normal, appBg: 'red' },
-    current: 'light'
   }
 }
