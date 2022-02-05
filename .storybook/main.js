691f9e9245137f9e0e2c637040e843321af1a89e
Daniel <2005danielus@gmail.com>
2022-02-05 11:22:42 +0100

    light/dark

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,7 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "storybook-addon-comments"
+    "storybook-dark-mode"
   ],
   "framework": "@storybook/react"
 }
